93910f59e6acf2deebb4fcce7cd9861fdd234c3e
berlianaputri8 <76423044+QistiIzatus@users.noreply.github.com>
2022-10-07 15:49:23 +0700

    edit file tambah

--- tambah.php
+++ tambah.php
@@ -19,53 +19,51 @@ if (isset($_POST['tambah'])) {
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tambah Data Buku</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Daftar Buku</title>
 </head>
-
 <body>
-  <h3>Form Tambah Data Buku</h3>
-  <form action="" method="POST">
-    <ul>
-      <li>
-        <label>
-         Nama Buku :
-          <input type="text" name="nama_buku" autofocus required>
-        </label>
-      </li>
-      <li>
-        <label>
-          Penulis Buku :
-          <input type="text" name="penulis_buku" required>
-        </label>
-      </li>
-      <li>
-        <label>
-          Tahun Penerbit :
-          <input type="text" name="tahun_penerbit" required>
-        </label>
-      </li>
-      <li>
-        <label>
-          Stok :
-          <input type="text" name="stok" required>
-        </label>
-      </li>
-      <li>
-        <label>
-          Gambar :
-          <input type="text" name="gambar" required>
-        </label>
-      </li>
-      <li>
-        <button type="submit" name="tambah">Tambah Data!</button>
-      </li>
-    </ul>
-  </form>
-</body>
+    <h3>Form Tambah Daftar Buku</h3>
 
+    <form action="" method="POST">
+        <ul>
+            <li>
+                <label>
+                    Judul Buku :               
+                    <input type="text" name="nama_buku" autofocus required>  
+                </label>            
+            </li>
+            <li>
+                <label>
+                   penulis buku:  
+                    <input type="text" name="penulis_buku" required>              
+                </label>    
+            </li>
+            <li>
+                <label>
+                    tahun penerbit :      
+                    <input type="text" name="tahun_penerbit" required>           
+                </label>           
+            </li>
+            <li>
+                <label>
+                    stok :      
+                    <input type="text" name="stok" required>           
+                </label>           
+            </li>
+            <li>
+                <label>
+                    Gambar :   
+                    <input type="text" name="gambar" required>              
+                </label>    
+            </li>
+            <li>
+                <button type="submit" name="tambah">Tambah Data!</button>
+            </li>
+        </ul>
+    </form>
+    
+</body>
 </html>
